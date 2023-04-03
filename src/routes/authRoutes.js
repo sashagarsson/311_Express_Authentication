1a2607c521fcaedccc8f30e71e71c545383e0b2b
sashagarsson <sasha.garsson@gmail.com>
2023-04-03 10:52:17 -0500

    final

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const authController = require("../controllers/authController");
+const auths = require("../middleware/auths");
 const router = express.Router();
+
 
  router.post('/register', authController.register);
 router.post('/login', authController.login); 
